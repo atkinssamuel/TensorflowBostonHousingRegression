3327a3672d3dbdcfb4c51ca2292f42c6f04254a7
Samuel Atkins <s.m.atkins73@gmail.com>
2020-03-09 19:14:47 -0400

    09/03/2020-Atkins-19:14:47 Updated training file with more rigorous comments and more stable procedure

--- project/train.py
+++ project/train.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 import numpy as np
 
+checkpoint_dir = "weights/"
+
 def train(x_train, y_train):
+    y_train = y_train.reshape(np.shape(y_train)[0], np.shape(y_train[1]))
+
     # Parameters:
     input_nodes = np.shape(x_train)[1]
     hidden_layer_1 = 1
@@ -9,25 +13,33 @@ def train(x_train, y_train):
     num_epochs = 3
     batch_size = 64
 
+    # Defining Layers:
+    # Placeholder for batch of inputs:
     x = tf.placeholder(tf.float32, [None, input_nodes])
+    # Layer 1 variables:
     W1 = tf.Variable(tf.truncated_normal([input_nodes, hidden_layer_1], stddev=0.15))
     b1 = tf.Variable(tf.zeros([hidden_layer_1]))
     y = tf.math.sigmoid(tf.matmul(x, W1) + b1)
+    # Placeholder for batch of targets:
+    y_ = tf.placeholder(tf.float32, [None, 1])
 
-    targets = np.reshape(y_train, (np.shape(y_train)[0], 1))
-    cost = tf.reduce_sum(tf.math.square(y - targets))
+    cost = tf.reduce_sum(tf.math.square(y - y_))
     predicted = tf.argmax(y, 1)
     optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
     # Miscellaneous quantities:
     sample_count = np.shape(x_train)[0]
 
+    # For weight saving:
+    saver = tf.train.Saver(max_to_keep=1)
+    checkpoint = "weights.ckpt"
+
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch_iteration in range(num_epochs):
-            for batch in range(int(sample_count)):
+            for batch in range(int(sample_count / batch_size)):
                 batch_x = x_train[batch * batch_size: (1 + batch) * batch_size]
-                print(batch_x.shape)
                 batch_y = y_train[batch * batch_size: (1 + batch) * batch_size]
-                sess.run([optimizer], feed_dict={x: batch_x, y: batch_y})
-
+                # Instantiating the inputs and targets with the batch values:
+                sess.run([optimizer], feed_dict={x: batch_x, y_: batch_y})
+        saver.save(sess, checkpoint)
